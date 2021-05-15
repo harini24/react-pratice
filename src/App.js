eb4bed5b16ef029eb524a56c9386a9bb50019d0b
harini <harinisiva1997@gmail.com>
2021-05-15 22:24:24 +0530

    modals added and practice proj done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUsers from './components/Users/AddUsers'
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const addUserhandler = (uname, age) => {
+    setUsers((prevUser) => {
+      return [...prevUser, { name: uname, age: age, id :Math.random().toString() }];
+    })
+  }
+
   return (
+  
     <div>
-      <AddUsers />
+      <AddUsers onAddUser={addUserhandler} />
+      <UsersList users={users} />
     </div>
   );
 }
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -2,14 +2,38 @@ import React,{ useState } from 'react'
 import Card from '../UI/Card'
 import Button from '../UI/Button'
 import classes from './AddUsers.module.css'
+import Modal from '../UI/Modal'
 const AddUsers = props => {
     const [username,setUsername] = useState('')
     const [age,setAge] = useState('')
+    const [error,setError] = useState()
     const adduserhandler = (e) => {
         e.preventDefault();
-        console.log(username,age)
+        if(username.trim().length ===0 || age.trim().length ===0){
+            setError({
+                title:"Invalud Input",
+                message:"Invalid username or age (non empty values)"
+            })
+            return;
+        }
+        if(age<1){
+            setError({
+                title:"Invalud Input",
+                message:"Please enter valid age above 0"
+            })
+            return;
+        }
+        props.onAddUser(username,age)
+        setUsername('')
+        setAge('')
+    }
+
+    const clearModal=()=>{
+    setError()
     }
     return (
+    <div>
+        {error && <Modal title={error.title} message={error.message} onclearModal={clearModal}></Modal>}
         <Card className={classes.input}>
             <form onSubmit={adduserhandler}>
                 <label htmlFor="username" >UserName</label>
@@ -22,6 +46,7 @@ const AddUsers = props => {
 
             </form>
         </Card>
+        </div>
     )
 }
 export default AddUsers
