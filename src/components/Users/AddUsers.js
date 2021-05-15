4c7c271b89e3c57fcf66e9022298cb8c39b509b6
harini <harinisiva1997@gmail.com>
2021-05-15 15:04:42 +0530

    reuasable button component and managing user input

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,23 +1,27 @@
+import React,{ useState } from 'react'
 import Card from '../UI/Card'
+import Button from '../UI/Button'
 import classes from './AddUsers.module.css'
-const addUsers = props => {
+const AddUsers = props => {
+    const [username,setUsername] = useState('')
+    const [age,setAge] = useState('')
     const adduserhandler = (e) => {
         e.preventDefault();
-
+        console.log(username,age)
     }
     return (
         <Card className={classes.input}>
             <form onSubmit={adduserhandler}>
                 <label htmlFor="username" >UserName</label>
-                <input id="username" type="text" />
+                <input id="username" value={username} onChange={(e)=>{setUsername(e.target.value)}} type="text" />
 
                 <label htmlFor="age" >Age (years)</label>
-                <input id="age" type="number" />
+                <input id="age" value={age} onChange={(e)=>{setAge(e.target.value)}} type="number" />
 
-                <button type="submit" >Add user</button>
+                <Button type="submit" >Add user</Button>
 
             </form>
         </Card>
     )
 }
-export default addUsers
+export default AddUsers
